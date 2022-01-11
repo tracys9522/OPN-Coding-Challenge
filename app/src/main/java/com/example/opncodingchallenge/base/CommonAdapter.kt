aaf47bb19c3cc064720e3bb54d02678b9846801f
Tracy Sun <gefeiyang.sun@itmx.co.th>
2022-01-11 17:26:29 +0700

    [mod] viewBinding adapter abstract methods

--- app/src/main/java/com/example/opncodingchallenge/base/CommonAdapter.kt
+++ app/src/main/java/com/example/opncodingchallenge/base/CommonAdapter.kt
@@ -4,18 +4,29 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 
-class CommonAdapter<T : Any, VB : ViewBinding> : RecyclerView.Adapter<CommonViewHolder<VB>>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommonViewHolder<VB> {
-        TODO("Not yet implemented")
-    }
+abstract class CommonAdapter<T : Any, VB : ViewBinding> :
+    RecyclerView.Adapter<CommonViewHolder<VB>>() {
+    open var mDatas: MutableList<T> = mutableListOf()
+
+    abstract override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommonViewHolder<VB>
 
     override fun onBindViewHolder(holder: CommonViewHolder<VB>, position: Int) {
-        TODO("Not yet implemented")
+        onBindViewHolder(
+            holder,
+            holder.adapterPosition,
+            holder.binding,
+            mDatas[holder.adapterPosition]
+        )
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    abstract fun onBindViewHolder(
+        holder: CommonViewHolder<VB>,
+        adapterPosition: Int,
+        binding: VB,
+        data: T
+    )
+
+    override fun getItemCount() = mDatas.size
 }
 
 open class CommonViewHolder<VB : ViewBinding>(val binding: VB) :
